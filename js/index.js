5f69615185b267a6bfa039b9842b7a9403eaa01b
jesus-rangel <jesusr.nm@gmail.com>
2021-03-01 14:05:38 -0400

    Almost done

--- js/index.js
+++ js/index.js
@@ -11,3 +11,27 @@ function getTitle() {
 getTitle();
 
 window.onresize = getTitle;
+
+/* **** TIPPY INSTANCES */
+
+tippy.setDefaultProps({ animation: "scale" });
+
+tippy(".james", { content: "Morgan James" });
+
+tippy(".jeanne", { content: "Jeanne de Clisson" });
+
+tippy(".frazier", { content: "Joe Frazier" });
+
+tippy(".churchill", { content: "Winston Churchill" });
+
+tippy(".pizarnik", { content: "Alejandra Pizarnik" });
+
+tippy(".ella", { content: "Ella Fitzgerald" });
+
+tippy(".tina", { content: "Tina Chang" });
+
+tippy(".coltrane", { content: "John Coltrane" });
+
+tippy(".karr", { content: "Mary Karr" });
+
+tippy("#chromium-browser", { content: "Download Chromium" });
